1265e5a42db5e272da2c06dc23a108bab6c2739c
Marten Stockenberg <MStockenberg@googlemail.com>
2018-02-15 11:17:46 +0100

    lightbox: open(), pagination, activeImg funcs are ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,63 @@
     // b. get the number of total imgs
     // get number of actual img
 
+/*
+    IMG Count
+    Next Button
+    Prev Button
+    Actual URL - after click
+    reference between img number and img in array
+ */
+
+var galleryItems = document.getElementById('gallery').children[0].children;
+
+/**
+ * Generate Pagination
+ */
+function generatePagination () {
+
+    $('.counter').empty()
+
+    $(galleryItems).map(function (index) {
+        var actualImgCount = index + 1;
+        $(galleryItems[index]).find('img').attr('data-index', index);
+        console.log(index + 1);
+        console.log(this);
+        $('.counter').append('<a href="#" data-index="'+index+'"> ' + actualImgCount + '</a>');
+    });
+
+    // Todo : next + prev buttons
+
+    /*
+    for(var i = 0; i < galleryItems.length; i++){
+        console.log(i);
+        console.log(galleryItems[i]);
+    }
+    */
+
+}
+
+$(document).ready(function () {
+
+    generatePagination();
+
+    /**
+     * click on pagination-link - to get img at index n
+     */
+    $('.counter a').click(function (){
+        console.log(galleryItems[this.dataset.index]);
+    } );
+
+    $('#gallery figure img').click(function () {
+        console.log(this.src);
+        var src = this.src;
+
+        $('#lightbox').fadeIn(500);
+        $('#lightbox figure img').attr('src', src);
+
+        $('.counter')
+            .find('a[data-index=' + this.dataset.index + ']')
+            .css('color', 'red');
+
+    });
+});
